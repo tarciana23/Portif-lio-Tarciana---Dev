162f7af09221ac31e8688042e442920c3c5b32a3
Tarciana Oliveira <anarumiroan@gmail.com>
2024-01-27 23:47:35 -0300

    alterando titulo projetos

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -2,7 +2,7 @@ import "../styles/components/projetos.sass"
 const Projetos = ()=>{
     return(
         <section className="projetos-container">
-            <h2>Meus projetos</h2>
+            <h2>Meus principais projetos</h2>
             <div className="listaProjetos">
                 <li><a href="https://github.com/tarciana23/Flappy-Bird">Flappy Bird</a></li>
                 <li><a href="https://formulario-js-chi.vercel.app/">Formulário JS</a></li>
