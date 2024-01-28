2312cd4e35562e368301eddb9db81aa7588c689a
Tarciana Oliveira <anarumiroan@gmail.com>
2024-01-28 19:52:06 -0300

    ajuste abouContainer

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,8 +3,9 @@ const AboutContainer = () => {
         <section className="about-container">
             <h2>Sobre</h2>
             <p>
-                Graduada em Ciência da Computação na UESB, apaixonada por tecnologia e pelo seu poder de revolucionar pessoas e realidades ,
-                atualmente sou desenvolvedora Web na Secretaria Municipal de Saúde de Vitória da Conquista - Ba e faço tecnólogo em Banco de Dados.
+            Formada em Ciência da Computação pela UESB, tenho uma paixão pela tecnologia e sua capacidade transformadora de pessoas 
+            e realidades. Atualmente, desempenho a função de desenvolvedora web na Secretaria Municipal de Saúde de Vitória da Conquista - Bahia, 
+            e estou cursando Tecnólogo em Banco de Dados.
             </p>
         </section>
 
